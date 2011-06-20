f4d03614021c2a85e11f1142e14b49cd60b74fcf
Rafael Novello <rafa.reis.novello@gmail.com>
2011-06-20 18:43:09 -0300

    Only small adjusts at admin interface

--- dataentry/admin.py
+++ dataentry/admin.py
@@ -2,9 +2,20 @@ from bvsnet.dataentry.models import *
 #from bvsnet.dataentry import 
 from django.contrib import admin
 
-admin.site.register(Instance)
+class InstanceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'portal_type')
+    
+class EvaluationAdmin(admin.ModelAdmin):
+    list_display = ('instance', 'avaliator', 'date')
+    
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'institution')
+    
+
+
+admin.site.register(Instance, InstanceAdmin)
 admin.site.register(Responsible)
-admin.site.register(Evaluation)
+admin.site.register(Evaluation, EvaluationAdmin)
 admin.site.register(InformationSource)
 admin.site.register(Server)
-admin.site.register(Contact)
+admin.site.register(Contact, ContactAdmin)

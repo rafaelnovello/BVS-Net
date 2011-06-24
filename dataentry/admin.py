c5b04d50ac7fc7040e6cab385d2f71d3c12d3134
Rafael Novello <rafa.reis.novello@gmail.com>
2011-06-24 01:28:15 -0300

    Adjusted dataentry admin to better presentation

--- dataentry/admin.py
+++ dataentry/admin.py
@@ -5,17 +5,28 @@ from django.contrib import admin
 class InstanceAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'portal_type')
     
+class InstanceNotesAdmin(admin.ModelAdmin):
+    list_display = ('instance', 'note')
+    
 class EvaluationAdmin(admin.ModelAdmin):
     list_display = ('instance', 'avaliator', 'date')
     
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('name', 'institution')
     
+class ContactPhoneAdmin(admin.ModelAdmin):
+    list_display = ('contact', 'phone')
+
+class ContactEmailAdmin(admin.ModelAdmin):
+    list_display = ('contact', 'email')
 
 
 admin.site.register(Instance, InstanceAdmin)
+admin.site.register(InstanceNotes, InstanceNotesAdmin)
 admin.site.register(Responsible)
 admin.site.register(Evaluation, EvaluationAdmin)
 admin.site.register(InformationSource)
 admin.site.register(Server)
 admin.site.register(Contact, ContactAdmin)
+admin.site.register(ContactPhone, ContactPhoneAdmin)
+admin.site.register(ContactEmail, ContactEmailAdmin)

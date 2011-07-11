48f86acd58f80dd381e69c03fd766d62ba3ab1f4
Rafael Novello <rafa.reis.novello@gmail.com>
2011-07-11 16:17:09 -0300

    Using Geraldo Reports to generate PDF example

--- dataentry/views.py
+++ dataentry/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.http import HttpResponse
+
+from reports import RelatorioListagem
+from geraldo.generators import PDFGenerator
+from models import Instance
+
+def testReport(request):
+    resp = HttpResponse(mimetype='application/pdf')
+    
+    instances = Instance.objects.order_by('name', 'id')
+    rel = RelatorioListagem(queryset=instances)
+    rel.generate_by(PDFGenerator, filename=resp)
+    
+    return resp
+    
+    
